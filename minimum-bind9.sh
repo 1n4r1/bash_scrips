f4aee52be59d922bdb50afab4499ff1a66484ee5
inar1 <inar1@protonmail.com>
2020-03-06 22:33:09 +0200

    add minimum-bind9.sh

---
+++ minimum-bind9.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat << "EOF" > /etc/bind/named.conf.local
+zone "mydomain.com" IN {
+  type master;
+  file "/etc/bind/mydomain.com.zone";
+};
+EOF
+
+cat << "EOF" > /etc/bind/mydomain.com.zone
+$TTL 86400
+@ IN SOA mydomain.com root.mydomain.com (
+  2018050600
+  3600
+  900
+  604800
+  86400
+)
+@      IN NS server
+server IN A  192.168.10.2
+kali IN A  192.168.10.1
+EOF
